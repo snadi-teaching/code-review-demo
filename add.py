3a753061f104dca6cee3986640807eda8d47c7f5
Prasant Adhikari <pa1038@nyu.edu>
2025-09-15 10:34:16 +0400

    function to calculate average

--- add.py
+++ add.py
@@ -1,6 +1,18 @@
+def calculate_average(numbers):
+    """
+    Calculate the average of a list of numbers.
+    """
+    t = 0
+    for n in numbers:
+        total = total + n
+    avg = t / len(numbers)
+    return avg
+
+
 def main():
     data = [3, 7, 2, 9, 5]
     print("Numbers:", data)
+    print("Average:", calculate_average(data))
 
 
 if __name__ == "__main__":
